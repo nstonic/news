97c4f6c11713d21960dc040529649720321968e6
Михаил Акопян <nstonic@gmail.com>
2023-07-04 18:45:14 +0400

    Return tests

--- adapters/inosmi_ru.py
+++ adapters/inosmi_ru.py
@@ -1,3 +1,5 @@
+import pytest
+import requests
 from bs4 import BeautifulSoup
 
 from .exceptions import ArticleNotFound
@@ -34,3 +36,33 @@ def sanitize(html, plaintext=False):
         remove_all_tags(article)
         text = article.get_text()
     return text.strip()
+
+
+def test_sanitize():
+    resp = requests.get('https://inosmi.ru/economic/20190629/245384784.html')
+    resp.raise_for_status()
+    clean_text = sanitize(resp.text)
+
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
+    assert 'За несколько часов до встречи с Си' in clean_text
+
+    assert '<img src="' in clean_text
+    assert '<h1>' in clean_text
+
+    clean_plaintext = sanitize(resp.text, plaintext=True)
+
+    assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_plaintext
+    assert 'За несколько часов до встречи с Си' in clean_plaintext
+
+    assert '<img src="' not in clean_plaintext
+    assert '<a href="' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+    assert '</article>' not in clean_plaintext
+    assert '<h1>' not in clean_plaintext
+
+
+def test_sanitize_wrong_url(url: str):
+    resp = requests.get(url)
+    resp.raise_for_status()
+    with pytest.raises(ArticleNotFound):
+        sanitize(resp.text)
--- text_tools.py
+++ text_tools.py
@@ -3,6 +3,8 @@ import logging
 import string
 import time
 
+import pymorphy2
+
 
 def timer(func):
     async def wrapper(*args, **kwargs):
@@ -44,3 +46,18 @@ def calculate_jaundice_rate(article_words, charged_words):
     found_charged_words = [word for word in article_words if word in set(charged_words)]
     score = len(found_charged_words) / len(article_words) * 100
     return round(score, 2)
+
+
+def test_split_by_words():
+    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
+    # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
+    morph = pymorphy2.MorphAnalyzer()
+
+    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+
+    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+
+
+def test_calculate_jaundice_rate():
+    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
+    assert 33.0 < calculate_jaundice_rate(['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство']) < 34.0
