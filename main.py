98d706a33c0145c98522bffbfbf6f868a6061a94
Михаил Акопян <nstonic@gmail.com>
2023-07-04 09:15:47 +0400

    Client ready

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import os
 from enum import Enum
 from pprint import pprint
@@ -6,36 +8,33 @@ from typing import List
 import aiohttp
 
 import anyio
-import bs4
 import pymorphy2
 from aiohttp import ClientResponseError
-from anyio import create_task_group
+from async_timeout import timeout
 
+from adapters import ArticleNotFound
+from adapters.inosmi_ru import sanitize
 from text_tools import split_by_words, calculate_jaundice_rate
 
 
 class JaundiceRater:
 
-    def __init__(self, urls: List[str]):
-        self.urls = urls
+    def __init__(self, dict_path: str):
         self.results = []
         self.charged_words = []
-        self.get_charged_words()
+        self.collect_charged_words(dict_path)
 
-    async def rate(self):
-        async with create_task_group() as tg:
-            for url in self.urls:
-                tg.start_soon(self.rate_article, url)
+    async def rate(self, urls: List[str]):
+        async with anyio.create_task_group() as tg:
+            for url in urls:
+                tg.start_soon(self.process_article, url)
 
     async def get_article_text(self, url: str):
         async with aiohttp.ClientSession() as self.session:
             html = await self.fetch(url)
-        soup = bs4.BeautifulSoup(html, 'lxml')
-        paragraphs = soup.find('div', class_='article__body').find_all('div', class_='article__text')
-        text = '\n'.join([p.text for p in paragraphs])
-        return text
+        return sanitize(html)
 
-    def get_charged_words(self, dict_path: str = 'charged_dict'):
+    def collect_charged_words(self, dict_path: str):
         for file in os.listdir(dict_path):
             with open(os.path.join(dict_path, file), encoding='utf8') as f:
                 self.charged_words.extend([
@@ -47,32 +46,45 @@ class JaundiceRater:
             response.raise_for_status()
             return await response.text()
 
-    async def rate_article(self, url: str):
+    async def process_article(self, url: str):
         result = {
-            'url': url
+            'Желтушность': None,
+            'Количество слов': None
         }
         try:
-            text = await self.get_article_text(url)
+            async with timeout(3):
+                text = await self.get_article_text(url)
+            if not text:
+                raise ArticleNotFound
+            async with timeout(3):
+                words = await split_by_words(pymorphy2.MorphAnalyzer(), text)
         except ClientResponseError:
             result.update({
-                'Статус': ProcessingStatus.FETCH_ERROR,
-                'rate': None,
-                'words_count': None
+                'Статус': ProcessingStatus.FETCH_ERROR
+            })
+        except asyncio.exceptions.TimeoutError:
+            result.update({
+                'Статус': ProcessingStatus.TIMEOUT
+            })
+        except ArticleNotFound:
+            result.update({
+                'Статус': ProcessingStatus.PARSING_ERROR
             })
         else:
-            words = split_by_words(pymorphy2.MorphAnalyzer(), text)
             rate = calculate_jaundice_rate(words, self.charged_words)
             result.update({
                 'Статус': ProcessingStatus.OK,
-                'rate': rate,
-                'words_count': len(words)
+                'Желтушность': rate,
+                'Количество слов': len(words)
             })
-        self.results.append(result)
+        self.results.append({url: result})
 
 
 class ProcessingStatus(Enum):
     OK = 'OK'
     FETCH_ERROR = 'FETCH_ERROR'
+    PARSING_ERROR = 'PARSING_ERROR'
+    TIMEOUT = 'TIMEOUT'
 
     def __str__(self):
         return str(self.value)
@@ -86,8 +98,9 @@ def main():
         'https://inosmi.ru/20230701/fobii-264001674.html',
         'https://inosmi.ru/20230701/britaniya-2640104.html'
     ]
-    rater = JaundiceRater(urls)
-    anyio.run(rater.rate)
+    # logging.basicConfig(level=logging.INFO)
+    rater = JaundiceRater('charged_dict')
+    anyio.run(rater.rate, urls)
     pprint(rater.results)
 
 
--- text_tools.py
+++ text_tools.py
@@ -1,5 +1,18 @@
-import pymorphy2
+import asyncio
+import logging
 import string
+import time
+
+
+def timer(func):
+    async def wrapper(*args, **kwargs):
+        started_at = time.monotonic()
+        result = await func(*args, **kwargs)
+        finished_at = time.monotonic()
+        logging.info(f'Анализ закончен за {finished_at - started_at} сек.')
+        return result
+
+    return wrapper
 
 
 def _clean_word(word):
@@ -9,7 +22,8 @@ def _clean_word(word):
     return word
 
 
-def split_by_words(morph, text):
+@timer
+async def split_by_words(morph, text):
     """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
     words = []
     for word in text.split():
@@ -17,17 +31,8 @@ def split_by_words(morph, text):
         normalized_word = morph.parse(cleaned_word)[0].normal_form
         if len(normalized_word) > 2 or normalized_word == 'не':
             words.append(normalized_word)
+        await asyncio.sleep(0)
     return words
-
-
-def test_split_by_words():
-    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
-    # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
-    morph = pymorphy2.MorphAnalyzer()
-
-    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
-
-    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
 
 
 def calculate_jaundice_rate(article_words, charged_words):
@@ -37,12 +42,5 @@ def calculate_jaundice_rate(article_words, charged_words):
         return 0.0
 
     found_charged_words = [word for word in article_words if word in set(charged_words)]
-
     score = len(found_charged_words) / len(article_words) * 100
-
     return round(score, 2)
-
-
-def test_calculate_jaundice_rate():
-    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
-    assert 33.0 < calculate_jaundice_rate(['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство']) < 34.0
